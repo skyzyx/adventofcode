eb72e8acf7ffbfb9b4032a8fd8d01215fdc81f2a
Ryan Parman <ryan@ryanparman.com>
2023-11-30 13:06:16 -0700

    2022/1

---
+++ 2022/1/solution.js
@@ -0,0 +1,12 @@
+function calories(input) {
+  return input
+    .map((e, i) => {
+      return {
+        k: i + 1,
+        v: e.reduce((a, e) => a + e),
+      };
+    })
+    .sort((a, b) => b.v - a.v)[0].k;
+}
+
+module.exports = calories;
